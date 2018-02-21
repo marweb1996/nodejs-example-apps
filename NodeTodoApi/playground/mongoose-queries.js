9e0ee8901d13466a2fbb3df9e60e29658762b659
Markus Weber <markus.weber@student.tugraz.at>
2018-02-21 06:32:31 +0100

    Add playground test for mongoose queries

---
+++ NodeTodoApi/playground/mongoose-queries.js
@@ -0,0 +1,28 @@
+const {ObjectId} = require("mongodb");
+const {mongoose} = require("./../server/db/mongoose");
+const {Todo} = require("./../server/models/todo");
+
+var id = "5a8d0017ccd7dc20843ad37411";
+
+if(!ObjectId.isValid(id)) {
+    console.log("Id not valid");
+} else {
+    Todo.find({
+        _id: id
+    }).then((todos) => {
+        console.log("Todos", todos);
+    });
+
+    Todo.findOne({
+        _id: id
+    }).then((todo) => {
+        console.log("Todo", todo);
+    });
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return console.log("Id not found");
+        }
+        console.log("Todo by Id", todo);
+    }).catch((err) => console.log(err));
+}
