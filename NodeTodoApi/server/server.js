3f5f6f9f30a4f53beda1b464726a83b15717861e
Markus Weber <markus.weber@student.tugraz.at>
2018-02-23 06:57:07 +0100

    Add PATCH /todos/:id

--- NodeTodoApi/server/server.js
+++ NodeTodoApi/server/server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 const {ObjectId} = require("mongodb");
 
 var {mongoose} = require("./db/mongoose")
@@ -63,7 +64,33 @@ app.delete("/todos/:id", (req, res) => {
 
         res.send({todo});
     }).catch((err) => {
-        res.status(err).send();
+        res.status(400).send();
+    });
+});
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ["text", "completed"]);
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body},  {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
