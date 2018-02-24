47f68a09f7680af4885ffebe0638381a0d5452d5
Markus Weber <markus.weber@student.tugraz.at>
2018-02-24 07:10:22 +0100

    Add GET /users/me and authentication middleware

--- NodeTodoApi/server/server.js
+++ NodeTodoApi/server/server.js
@@ -7,6 +7,7 @@ const {ObjectId} = require("mongodb");
 var {mongoose} = require("./db/mongoose")
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {authenticate} = require("./middleware/authenticate");
 
 var app = express();
 
@@ -111,6 +112,10 @@ app.post("/users", (req, res) => {
     .catch((err) => {
         res.status(400).send(err);
     });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
