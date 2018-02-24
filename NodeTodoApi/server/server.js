1992a8012d9ea0e157582875f1233821c07fedf0
Markus Weber <markus.weber@student.tugraz.at>
2018-02-24 06:46:39 +0100

    Add POST /users route and token method

--- NodeTodoApi/server/server.js
+++ NodeTodoApi/server/server.js
@@ -14,6 +14,8 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//------------------------------------------------------------------
+// Todos routes
 app.post("/todos", (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -92,6 +94,22 @@ app.patch("/todos/:id", (req, res) => {
         res.send({todo});
     }).catch((err) => {
         res.status(400).send();
+    });
+});
+//------------------------------------------------------------------
+
+//------------------------------------------------------------------
+// User routes
+app.post("/users", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"]);
+    var user = new User(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header("x-auth", token).send(user);
+    })
+    .catch((err) => {
+        res.status(400).send(err);
     });
 });
 
