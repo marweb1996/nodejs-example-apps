54e985b158d54d9e8f1dd498506f729145f9934a
Markus Weber <markus.weber@student.tugraz.at>
2018-02-25 07:16:49 +0100

    Add POST /users/login route

--- NodeTodoApi/server/server.js
+++ NodeTodoApi/server/server.js
@@ -118,6 +118,18 @@ app.get("/users/me", authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post("/users/login", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"]);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header("x-auth", token).send(user);
+        })
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
