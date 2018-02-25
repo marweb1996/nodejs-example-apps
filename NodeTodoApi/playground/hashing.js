3ca896a6032451f861299dfa70dec0badcd9bc9f
Markus Weber <markus.weber@student.tugraz.at>
2018-02-25 06:01:08 +0100

    Hash user password before saving

--- NodeTodoApi/playground/hashing.js
+++ NodeTodoApi/playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-    id: 10
-}
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
-var decoded = jwt.verify(token, "123abc");
-console.log("decoded", decoded);
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = "$2a$10$.M9EP.9M8JsyuCFV2DvPXeDkQRpnmbgxWjrosAvoSGW1u74pZidDW";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+// var decoded = jwt.verify(token, "123abc");
+// console.log("decoded", decoded);
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
