32a93c432a881947a7d2ec0c65093aec66fba53d
Markus Weber <markus.weber@student.tugraz.at>
2018-02-25 20:37:45 +0100

    Associate users with todos

--- NodeTodoApi/server/tests/seed/seed.js
+++ NodeTodoApi/server/tests/seed/seed.js
@@ -20,19 +20,26 @@ const users = [
     {
         _id: userTwoId,
         email: "patrick@example.com",
-        password: "userTwoPass"
+        password: "userTwoPass",
+        tokens: [{
+            access: "auth",
+            token: jwt.sign({_id: userTwoId, access: "auth"}, "abc123").toString()
+        }]
     }
 ];
 
 const todos = [
     {
         _id: new ObjectId(),
-        text: "First test todo"},
+        text: "First test todo",
+        _creator: userOneId
+    },
     {
         _id: new ObjectId(), 
         text: "Second test todo",
         completed: true,
-        completedAt: 333
+        completedAt: 333,
+        _creator: userTwoId
     }
 ];
 
